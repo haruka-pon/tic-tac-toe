5f6d9ab641028238bc60fa057bebca00a522178d
mosapon <1127.yagiha@ezweb.ne.jp>
2023-05-23 10:18:17 +0900

    add app.js function Cubemaler

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function Square({ value, onSquareClick }) {
  *  3. Board コンポーネントの state である squares が更新されたので、Board とそのすべての子が再レンダーされます。これにより、インデックス 0 である Square コンポーネントの value プロパティが null から X に変更されます。
 */
 
-export default function Board() {
+export function TicTacToe() {
 
   const [xIsNext, setXIsNext] = useState(true);
   // どちらのプレーヤの手番なのかを決める xIsNext
@@ -98,3 +98,29 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+export function CubeMaker() {
+  const cubeArray =
+    [
+      { height: 10, width: 10, depth: 10 },
+      { height: 2, width: 3, depth: 2 },
+      { height: 4, width: 4, depth: 2 },
+      { height: 3, width: 3, depth: 3 },
+      { height: 1, width: 11, depth: 5 },
+      { height: 5, width: 2, depth: 5 },
+      { height: 5, width: 5, depth: 5 },
+    ];
+
+  const addCube = [{ height: 1, width: 1, depth: 1 }];
+
+  cubeArray.push(addCube);
+
+  cubeArray.map((num) => {
+    return (
+      <>
+        <p>{num}</p>
+      </>
+    );
+  });
+}
+
